d27d93ac66b5edf88a9903328bbbe37d50d5f6e9
Siva Chandran <sivavchandran@gmail.com>
2022-09-25 00:30:14 +0530

    feat: update logger

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,12 +1,17 @@
 const winston = require('winston');
 require('winston-papertrail').Papertrail;
+const dotenv = require('dotenv');
+dotenv.config;
 
 const { createLogger, format, transports } = winston;
 const { combine, printf, timestamp, colorize, prettyPrint } = format;
 
+const host = process.env.LOGGER_HOST;
+const port = process.env.LOGGER_PORT;
+
 const paperLogger = new winston.transports.Papertrail({
-  host: 'logs2.papertrailapp.com',
-  port: 16244,
+  host,
+  port,
   inlineMeta: true,
   logFormat: function (level, message) {
     if (level === 'error') {
